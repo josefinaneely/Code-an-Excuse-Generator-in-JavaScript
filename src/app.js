2ab4547689983dbddb464c30f39c9ba9eb357244
josefina neely <joseneelyg@gmail.com>
2025-04-22 15:28:37 +0000

    code and excuse terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function generateExcuse() {
+  let who = ['Mi bebé', 'Mi abuela', 'El gato', 'La lagartija'];
+  let action = ['vomitó', 'orinó', 'rompió', 'cortó'];
+  let what = ['mi tarea', 'mi ropa', 'mi casa', 'mi cama'];
+  let when = ['antes de salir', 'cuando dormía', 'mientras comíamos', 'durante el almuerzo', 'cuando rezabamos'];
+
+  let excuse = randomElement(who) + " " + randomElement(action) + " " + randomElement(what) + " " + randomElement(when);
+  return excuse;
+}
+
+
+window.onload = function () {
+document.getElementById("excuse").innerHTML = generateExcuse()
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(generateExcuse());
 };
